670f6f3a3c2757489f8e7c902606485596e0c46a
Devanshu Vishwakarma <151915434+devv1310@users.noreply.github.com>
2025-12-19 10:23:16 +0000

    deploy ready

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://collaborative-task-manager-g6te.onrender.com/",
   withCredentials: true, // VERY IMPORTANT (JWT cookies)
 });
